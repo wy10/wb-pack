6f0b55db07df1576b283bb5a4bd6c0eec0459d4e
wenyan <wenyan@go-goal.com>
2022-06-27 15:32:04 +0800

    fix: 注释

--- bin/wb-pack.js
+++ bin/wb-pack.js
@@ -2,6 +2,7 @@
 
 // 1.读取配置文件
 const path = require("path")
+// 如果在处理完所有给定的 path 片段之后，还没有生成绝对路径，则使用当前工作目录
 const config = require(path.resolve('webpack.config.js'))
 // 2.通过面向对象的方式来进行项目推进
 const Compiler = require("../lib/Compiler")
